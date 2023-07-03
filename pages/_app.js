566c0c94280b4f793427d32cd5b002e54e2c20b6
Omar Mohamed Elsayed <111247064+elsharkawy159@users.noreply.github.com>
2023-07-03 04:27:07 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -62,6 +62,11 @@ export default function MyApp({ Component, pageProps }) {
       />
       <Script src="https://unpkg.com/scrollreveal" />
       <Script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js" />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossOrigin="anonymous"
+      />
     </>
   );
 }
