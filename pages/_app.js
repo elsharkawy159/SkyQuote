cc9c112276b136e24fd2b098ea4a1349bd589a94
Omar Mohamed Elsayed <111247064+elsharkawy159@users.noreply.github.com>
2023-07-03 04:40:25 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -67,6 +67,11 @@ export default function MyApp({ Component, pageProps }) {
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossOrigin="anonymous"
       />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"
+      />
     </>
   );
 }
