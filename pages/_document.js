a3ad807f4d687887dcdee8aa7cdf11fc4361f3f9
Omar Mohamed Elsayed <elsharkawy159.om@gmail.com>
2023-07-04 23:51:34 +0300

    q

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,58 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Script } from "next/script"; // import Script component
 
-// import "bootstrap/dist/js/bootstrap.bundle.js";
-import "bootstrap/js/dist/dropdown.js"
-export default function Document() {
+import "bootstrap/js/dist/dropdown.js";
+
+function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charSet="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:wght@500;600;700;800&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Solitreo&family=Xanh+Mono:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+          crossorigin="anonymous"
+        />
+        <Script //MDB
+          type="text/javascript"
+          src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"
+        />
+        <Script src="https://unpkg.com/scrollreveal" />
+        <Script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js" />
       </body>
     </Html>
   );
 }
+
+export default Document;
