90c6c2a5d7f586b0a2832845ca9e39da3e9d5869
Omar Mohamed Elsayed <elsharkawy159.om@gmail.com>
2023-07-05 00:55:49 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Script } from "next/script"; // import Script component
 
-import "bootstrap/js/dist/dropdown.js";
-
 function Document() {
   return (
     <Html lang="en">
