fce8a0fcce7b98e2fb36edad27a0d256b60aecff
Omar Mohamed Elsayed <elsharkawy159.om@gmail.com>
2023-07-05 03:26:55 +0300

    eqwe

--- Components/MainLanding/MainLanding.jsx
+++ Components/MainLanding/MainLanding.jsx
@@ -1,37 +1,33 @@
 import React from "react";
-import style from "./MainLanding.module.css";
 import { QuoteCard, QuoteCardList } from "../QuoteCard/QuoteCard.jsx";
 
 export const MainLanding = () => {
   return (
     <section>
       <div
-        className={`container-fluid ${style.bg_main} d-flex align-items-center justify-content-start`}
+        className={`container-fluid bg_main vh-100 d-flex align-items-center justify-content-start`}
       >
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-6">
-                <h2 className="scriptFont text-light opacity-75">
-                  Add and Disover Amazing Quotes
-                </h2>
-                <h1 className="fw-bold text-light title"></h1>
-                <h1
-                  className="fw-bolder text-light mt-4"
-                  style={{ fontSize: "60px" }}
-                >
-                  Add Your{" "}
-                  <span className="text-dark title position-relative discountBg">
-                    Quote
-                  </span>{" "}
-                  Now
-                </h1>
+              <h2 className="scriptFont text-light opacity-75">
+                Add and Disover Amazing Quotes
+              </h2>
+              <h1 className="fw-bold text-light title"></h1>
+              <h1
+                className="fw-bolder text-light mt-4"
+                style={{ fontSize: "60px" }}
+              >
+                Add Your{" "}
+                <span className="text-dark title position-relative discountBg">
+                  Quote
+                </span>{" "}
+                Now
+              </h1>
             </div>
             <div className="col-md-6">
-
-                  <p className="text-light text-center mb-3">
-                    Latest Added Quote
-                  </p>
-                  <QuoteCardList col={"col-9"} length={1} />
+              <p className="text-light text-center mb-3">Latest Added Quote</p>
+              <QuoteCardList col={"col-9"} length={1} />
             </div>
           </div>
         </div>
